df2f93fb4248e4c655b32c512b56ecaf086fb769
Surya <suryaprataprana3@gmail.com>
2024-09-06 09:36:02 +0530

    set DEBUG = False

--- QuantumTwinTech/settings.py
+++ QuantumTwinTech/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-vt@&=01dq3)i%!!(0lwv)8n(t%ib*%@ezz@89zkxqvkj-_^g+%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
